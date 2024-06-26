e36c0141604b8000e53e8f6750d139703c43ec37
rogeeei <cuevasrogelyn2@gmail.com>
2024-06-27 00:36:24 +0800

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->text('firstname');
+            $table->text('middle_name')->nullable();
+            $table->text('lastname');
+            $table->text('suffix')->nullable();
+            $table->string('email')->unique();
+            $table->string('phone_number')->unique();
+            $table->date('birthdate')->nullable();
+            $table->string('brgy');
+            $table->string('role');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
